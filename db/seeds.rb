4d99eb447ea51fdbffab21b2f7d067c9927b6b87
Max Manayan <maximillian521@gmail.com>
2021-03-18 14:53:14 -0600

    initial commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+require 'faker'
+
+5.times do 
+  @patient = Patient.create(name: Faker::Name.name, age: Faker::Number.within(range: 1..100)) 
+end
+
+5.times do 
+  @doctor = Doctor.create(name: Faker::Name.name)
+
+  # @patient = Patient.create(name: Faker::Name.name, age: Faker::Number.within(range: 1..100)) 
+
+  6.times do |i|
+    Appointment.create(date: Faker::Date.forward(days: 23), description: Faker::Movies::StarWars.wookiee_sentence, doctor_id: @doctor.id, patient_id: i)
+  end
+end
+
+
+puts "Seeded #{Doctor.all.size} new doctors"
+puts "Seeded #{Patient.all.size} new patients"
+puts "Seeded #{Appointment.all.size} new appointments"
