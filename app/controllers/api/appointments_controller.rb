0ee88ec832f9ff454fad62d850f5f77af533ffc5
Max Manayan <maximillian521@gmail.com>
2021-03-18 15:17:28 -0600

    appointment controller setup

--- app/controllers/api/appointments_controller.rb
+++ app/controllers/api/appointments_controller.rb
@@ -1,2 +1,50 @@
 class Api::AppointmentsController < ApplicationController
+
+  before_action :get_appointment, only: [:show, :update, :destroy]
+  
+  def index
+    render json: Appointment.all
+  end
+
+
+  def show
+    render json: @appointment
+  end
+
+
+  def create
+    appointment = Appointment.new(appointment_params)
+    if appointment.save
+      render json: appointment
+    else
+      render json: {error: appointment.errors}, status: 422
+    end
+  end
+
+
+  def update 
+    if @appointment.update(appointment_params)
+      render json: @appointment
+    else
+      render json: {error: appointment.errors}, status: 422
+    end
+  end
+
+
+  def destroy
+    @appointment.destroy
+    render json: @appointment
+  end
+
+
+  private
+  
+  def appointment_params
+    params.require(:appointment).permit(:date, :description, :doctor_id, :patient_id)
+  end
+
+
+  def get_appointment
+    @appointment = Appointment.find(params[:id])
+  end
 end
